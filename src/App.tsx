c97d36236976618fe14358d1dbcd06947c4c39c3
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-03-17 04:30:40 +0530

    feat: more displays

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,39 @@ const Uploadable: React.FC = ({ children }) => {
 	return <Fragment>{children}</Fragment>
 }
 
-const ArrayHandler: React.FC<{ arr_data: Array<any>; firebase_collection: string; firebase_id: string }> = ({
+const ArrayHandler: React.FC<{ arr_data: Array<any>; path: string; firebase_collection: string; firebase_id: string }> = ({
 	arr_data,
 	firebase_collection,
 	firebase_id,
+	path,
 }) => {
 	// console.log(typeof arr_data?.[0] === 'object')
-	return (
-		<Fragment>
-			{arr_data.map((data) => {
-				return <DataHandler firebase_collection={firebase_collection} firebase_id={firebase_id} data={data} />
-			})}
-		</Fragment>
-	)
+	if (path === 'images') {
+		return (
+			<Fragment>
+				{arr_data.map((data, idx) => {
+					return (
+						<Uploadable>
+							<img style={{ width: '100%', maxWidth: '20	0px' }} src={data as string} alt='' />
+						</Uploadable>
+					)
+				})}
+			</Fragment>
+		)
+	} else
+		return (
+			<Fragment>
+				{arr_data.map((data, idx) => {
+					return <DataHandler firebase_collection={firebase_collection} firebase_id={firebase_id} data={data} path={path + '/idx_' + idx} />
+				})}
+			</Fragment>
+		)
 }
 
-const ObjectHandler: React.FC<{ obj_data: any; firebase_collection: string; firebase_id: string }> = ({
+const ObjectHandler: React.FC<{ path: string; obj_data: any; firebase_collection: string; firebase_id: string }> = ({
 	obj_data,
 	firebase_collection,
+	path,
 	firebase_id,
 }) => {
 	const keys = Object.keys(obj_data).sort()
@@ -49,6 +64,7 @@ const ObjectHandler: React.FC<{ obj_data: any; firebase_collection: string; fire
 							return (
 								<td>
 									<Editable
+										path={path + '/' + key}
 										firebase_collection={firebase_collection}
 										firebase_id={firebase_id}
 										text={obj_data[key] as string}
@@ -63,35 +79,45 @@ const ObjectHandler: React.FC<{ obj_data: any; firebase_collection: string; fire
 	)
 }
 
-const DataHandler: React.FC<{ data: any; firebase_collection: string; firebase_id: string }> = ({ data, firebase_collection, firebase_id }) => {
+const DataHandler: React.FC<{ data: any; path: string; firebase_collection: string; firebase_id: string }> = ({
+	data,
+	firebase_collection,
+	firebase_id,
+	path,
+}) => {
 	if (typeof data !== 'object') {
-		return <Editable firebase_collection={firebase_collection} firebase_id={firebase_id} text={data as string}></Editable>
+		return <Editable path={path} firebase_collection={firebase_collection} firebase_id={firebase_id} text={data as string}></Editable>
 	}
 	if (data instanceof Array) {
 		return (
 			<Fragment>
 				<td>
-					<ArrayHandler firebase_collection={firebase_collection} firebase_id={firebase_id} arr_data={data}></ArrayHandler>
+					<ArrayHandler path={path} firebase_collection={firebase_collection} firebase_id={firebase_id} arr_data={data}></ArrayHandler>
 				</td>
 			</Fragment>
 		)
-	} else return <ObjectHandler firebase_collection={firebase_collection} firebase_id={firebase_id} obj_data={data}></ObjectHandler>
+	} else return <ObjectHandler path={path} firebase_collection={firebase_collection} firebase_id={firebase_id} obj_data={data}></ObjectHandler>
 }
 
 const Editable: React.FC<{
 	text: string
 	firebase_collection: string
 	firebase_id: string
-}> = ({ text }) => {
+	path: string
+}> = ({ text, path, firebase_collection, firebase_id }) => {
 	const [clicked, setClicked] = useState<boolean>(false)
 	return (
 		<Fragment>
 			{clicked ? (
 				<Fragment>
-					<textarea cols={20} rows={10}>
-						{text}
-					</textarea>
-					<Button onClick={() => setClicked(!clicked)} variant='primary'>
+					<textarea cols={20} rows={10} defaultValue={text}></textarea>
+					<Button
+						onClick={() => {
+							setClicked(!clicked)
+							console.log(firebase_collection, firebase_id, path)
+						}}
+						variant='primary'
+					>
 						Submit
 					</Button>{' '}
 				</Fragment>
@@ -135,7 +161,7 @@ const Tester: React.FC<{ collection_name: string }> = ({ collection_name }) => {
 										if (images_single.includes(key)) {
 											return (
 												<Fragment key={elem[key] as string}>
-													<td style={{ maxWidth: '200px' }}>
+													<td style={{ width: '200px', maxWidth: '200px' }}>
 														<Uploadable>
 															<img style={{ width: '100%' }} src={elem[key] as string} alt='' />
 														</Uploadable>
@@ -145,11 +171,12 @@ const Tester: React.FC<{ collection_name: string }> = ({ collection_name }) => {
 										} else if (typeof elem[key] !== 'object')
 											return (
 												<Fragment key={elem[key] as string}>
-													<td style={{ maxWidth: '200px' }}>
+													<td style={{ maxWidth: '250px', width: '250px' }}>
 														{key === 'NO_ID_FIELD' ? (
 															(elem[key] as string)
 														) : (
 															<Editable
+																path={key}
 																firebase_collection={collection_name}
 																firebase_id={elem['NO_ID_FIELD'] as string}
 																text={elem[key] as string}
@@ -162,6 +189,7 @@ const Tester: React.FC<{ collection_name: string }> = ({ collection_name }) => {
 											return (
 												<Fragment>
 													<DataHandler
+														path={key}
 														firebase_collection={collection_name}
 														firebase_id={elem['NO_ID_FIELD'] as string}
 														data={elem[key] as any}
@@ -184,7 +212,7 @@ const App: React.FC = () => {
 	return (
 		<Fragment>
 			<FirebaseAppProvider firebaseConfig={config}>
-				<Tester collection_name='data'></Tester>
+				<Tester collection_name='items'></Tester>
 			</FirebaseAppProvider>
 		</Fragment>
 	)

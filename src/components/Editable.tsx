e4d635712905f6e784b7e1f1070b0395b0360643
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-03-17 04:59:12 +0530

    feat: update component

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import { Button } from 'react-bootstrap'
+import { useFirestore, useFirestoreDocDataOnce } from 'reactfire'
 
 export const Editable: React.FC<{
 	text: string
@@ -8,26 +9,61 @@ export const Editable: React.FC<{
 	path: string
 }> = ({ text, path, firebase_collection, firebase_id }) => {
 	const [clicked, setClicked] = useState<boolean>(false)
+
 	return (
 		<Fragment>
 			{clicked ? (
-				<Fragment>
-					<textarea cols={20} rows={10} defaultValue={text}></textarea>
-					<Button
-						onClick={() => {
-							setClicked(!clicked)
-							console.log(firebase_collection, firebase_id, path)
-						}}
-						variant='primary'
-					>
-						Submit
-					</Button>{' '}
-				</Fragment>
+				<UpdateComponent
+					text={text}
+					setClicked={setClicked}
+					clicked={clicked}
+					firebase_collection={firebase_collection}
+					firebase_id={firebase_id}
+					path={path}
+				/>
 			) : (
+				// UpdateComponent(text, setClicked, clicked, firebase_collection, firebase_id, path)
 				<div onClick={() => setClicked(!clicked)} className='' style={{ minHeight: '20px' }}>
 					<p style={{ width: '220px', whiteSpace: 'normal', overflow: 'hidden', textOverflow: 'ellipsis' }}>{text}</p>
 				</div>
 			)}
+		</Fragment>
+	)
+}
+
+const UpdateComponent: React.FC<{
+	text: string
+	setClicked: CallableFunction
+	clicked: boolean
+	firebase_collection: string
+	firebase_id: string
+	path: string
+}> = ({ text, setClicked, clicked, firebase_collection, firebase_id, path }) => {
+	const documentReference = useFirestore().collection(firebase_collection).doc(firebase_id)
+	const { status, data } = useFirestoreDocDataOnce(documentReference)
+	if (status === 'loading') {
+		console.log(status)
+	} else {
+		console.log(status)
+		console.log(data)
+	}
+
+	async function handleSubmit() {
+		// setClicked(!clicked)}
+		console.log(data)
+	}
+
+	return (
+		<Fragment>
+			<textarea cols={20} rows={10} defaultValue={text}></textarea>
+			<Button
+				onClick={() => {
+					handleSubmit()
+				}}
+				variant='primary'
+			>
+				Submit
+			</Button>{' '}
 		</Fragment>
 	)
 }

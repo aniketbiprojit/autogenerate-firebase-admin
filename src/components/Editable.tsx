97c4914604865419703c9960819baffa4887e1c1
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-03-17 05:26:03 +0530

    feat: firebase updates

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -1,6 +1,19 @@
 import React, { Fragment, useState } from 'react'
 import { Button } from 'react-bootstrap'
-import { useFirestore, useFirestoreDocDataOnce } from 'reactfire'
+import { useFirestore, useFirestoreDocData } from 'reactfire'
+
+import _ from 'lodash'
+
+// import {} from 'firebase'
+import firebase from 'firebase'
+
+import config from '../firebase_config.json'
+
+if (firebase.apps.length === 0) {
+	firebase.initializeApp(config)
+}
+
+const firestore = firebase.firestore()
 
 export const Editable: React.FC<{
 	text: string
@@ -15,8 +28,7 @@ export const Editable: React.FC<{
 			{clicked ? (
 				<UpdateComponent
 					text={text}
-					setClicked={setClicked}
-					clicked={clicked}
+					setClicked={() => setClicked(!clicked)}
 					firebase_collection={firebase_collection}
 					firebase_id={firebase_id}
 					path={path}
@@ -34,28 +46,47 @@ export const Editable: React.FC<{
 const UpdateComponent: React.FC<{
 	text: string
 	setClicked: CallableFunction
-	clicked: boolean
+
 	firebase_collection: string
 	firebase_id: string
 	path: string
-}> = ({ text, setClicked, clicked, firebase_collection, firebase_id, path }) => {
+}> = ({ text, setClicked, firebase_collection, firebase_id, path }) => {
+	const [text_data, setTextData] = useState(text)
+
 	const documentReference = useFirestore().collection(firebase_collection).doc(firebase_id)
-	const { status, data } = useFirestoreDocDataOnce(documentReference)
+	const { status, data } = useFirestoreDocData(documentReference)
+
 	if (status === 'loading') {
 		console.log(status)
 	} else {
 		console.log(status)
-		console.log(data)
+		// console.log(data)
 	}
 
 	async function handleSubmit() {
-		// setClicked(!clicked)}
-		console.log(data)
+		// console.log(_.get(data, path))
+		console.log(_.set(data as any, path, text_data))
+		// console.log(_.get(data, path))
+		// console.log(data)
+		console.log(
+			await firestore
+				.collection(firebase_collection)
+				.doc(firebase_id)
+				.update(data as any)
+		)
+		setClicked()
 	}
 
 	return (
 		<Fragment>
-			<textarea cols={20} rows={10} defaultValue={text}></textarea>
+			<textarea
+				cols={20}
+				rows={10}
+				onChange={(e) => {
+					setTextData(e.target.value)
+				}}
+				value={text_data}
+			></textarea>
 			<Button
 				onClick={() => {
 					handleSubmit()
